cad243f98cd1b4df6d90ef5bed7899b415e3eba8
gios91 <giuseppe.silvestri.gios@gmail.com>
2022-03-25 18:40:20 +0100

    Remove replace s3a to s3 into sanitizedTempDir

--- src/main/scala/io/github/spark_redshift_community/spark/redshift/RedshiftWriter.scala
+++ src/main/scala/io/github/spark_redshift_community/spark/redshift/RedshiftWriter.scala
@@ -320,13 +320,12 @@ private[redshift] class RedshiftWriter(
       }
       // It's possible that tempDir contains AWS access keys. We shouldn't save those credentials to
       // S3, so let's first sanitize `tempdir` and make sure that it uses the s3:// scheme:
-      val sanitizedTempDir = Utils.fixS3Url(
-        Utils.removeCredentialsFromURI(URI.create(tempDir)).toString).stripSuffix("/")
+      val sanitizedTempDir = Utils.removeCredentialsFromURI(URI.create(tempDir)).toString.stripSuffix("/")
       val manifestEntries = filesToLoad.map { file =>
         s"""{"url":"$sanitizedTempDir/$file", "mandatory":true}"""
       }
       val manifest = s"""{"entries": [${manifestEntries.mkString(",\n")}]}"""
-      val manifestPath = tempDir + "manifest.json"
+      val manifestPath = sanitizedTempDir + "manifest.json"
       val fsDataOut = fs.create(new Path(manifestPath))
       try {
         fsDataOut.write(manifest.getBytes("utf-8"))
